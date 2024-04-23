e889a9026f97da5a8f9ecae69d2b77140e9e0991
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-04-22 21:20:48 -0300

    Construção de catalogo de filmes

--- Curso/src/Main.java
+++ Curso/src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá mundo");
+        System.out.println("Esse é o Scren Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+
+        double notaDoFilme = 8.1;
+
+        //media calculada
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = "Filme de aventura com gala dos anos 80";
+        System.out.println(sinopse);
+
+
+
+        int classificacao;
+        classificacao = (int) (media / 2);
+        System.out.println(classificacao);
+
+
+
     }
 }
---
+++ Curso/src/exercicios/teste.java
@@ -0,0 +1,12 @@
+package exercicios;
+
+public class teste {
+
+    public static void main(String[] args) {
+        String saudacao = "Olá, meu nome é ";
+        String nome = "Alice ";
+        String continuacao = "e minha idade é ";
+        int idade = 17;
+        System.out.println(saudacao + nome + continuacao + idade);
+    }
+}
