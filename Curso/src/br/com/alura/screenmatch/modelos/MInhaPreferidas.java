268a32084919d5ae3b28c3f01a2dcf82e7a04ae0
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-05-07 20:31:12 -0300

    Finalizado curso java oo

---
+++ Curso/src/br/com/alura/screenmatch/modelos/MInhaPreferidas.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch.modelos;
+
+public class MInhaPreferidas {
+
+
+    public void inclui(Audio audio){
+        if(audio.getClassificacao() >= 9){
+            System.out.println(audio.getTitulo() + " é considerado sucesso absoluto e preferido por todos!");
+        }else{
+            System.out.println(audio.getTitulo() + " também é bom!");
+        }
+    }
+}
